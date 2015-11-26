7615aabec1eebabce8aed134aabb92bd81d31f69
Stephen Bucknall <steveb@eqnet.co.uk>
2015-11-26 16:01:03 +0400

    A checkin to my application

--- index.js
+++ index.js
@@ -197,11 +197,11 @@ function msgReceived(mongodbUser, users, msg) {
 	}
 }
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 server.listen(server_port, server_ip_address, function () {
-console.log( "Listening on " + server_ip_address + ", server_port " + port )
+	console.log( "Listening on " + server_ip_address + ", server_port " + port );
 });
 /*
 db.collection('users').count({nickname:msg}, function(err, count) {
