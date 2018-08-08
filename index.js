f5b29bfea5ed59d95ea9a701e05eb73da5c79f55
Steve Bucknall <steveb@eqnet.co.uk>
2018-08-08 14:18:40 +0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var io = require('socket.io').listen(server);
 var MongoClient = require('mongodb').MongoClient;
 
 var db;
-var uri = 'mongodb://localhost:27017/Chat';
+var uri = 'mongodb://dXNlck82Mg==:b3kyNG1sUkdtVTR0MXg3UQ==@localhost:27017/node';
 MongoClient.connect(uri, {useNewUrlParser:true}, function(err, client) {
    if (err) {
 	   console.log('Error conecting to MongoDB:' + err)
