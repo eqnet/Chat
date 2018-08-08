b9dfc38b439a07fe73bcc57d7b7c28bdc80a0acb
Steve Bucknall <steveb@eqnet.co.uk>
2018-08-08 22:10:09 +0400

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var server = app.listen(8080);
 var io = require('socket.io').listen(server);
 var MongoClient = require('mongodb').MongoClient;
 
-console.log('OPENSHIFT_MONGODB_DB_HOST' + env.OPENSHIFT_MONGODB_DB_HOST);
-console.log('OPENSHIFT_MONGODB_DB_USERNAME' + env.OPENSHIFT_MONGODB_DB_USERNAME);
-console.log('OPENSHIFT_MONGODB_DB_PASSWORD' + env.OPENSHIFT_MONGODB_DB_PASSWORD);
-console.log('OPENSHIFT_MONGODB_DB_HOST' + env.OPENSHIFT_MONGODB_DB_HOST);
-console.log('OPENSHIFT_MONGODB_DB_PORT' + env.OPENSHIFT_MONGODB_DB_PORT);
+console.log('OPENSHIFT_MONGODB_DB_HOST' + process.env.OPENSHIFT_MONGODB_DB_HOST);
+console.log('OPENSHIFT_MONGODB_DB_USERNAME' + process.env.OPENSHIFT_MONGODB_DB_USERNAME);
+console.log('OPENSHIFT_MONGODB_DB_PASSWORD' + process.env.OPENSHIFT_MONGODB_DB_PASSWORD);
+console.log('OPENSHIFT_MONGODB_DB_HOST' + process.env.OPENSHIFT_MONGODB_DB_HOST);
+console.log('OPENSHIFT_MONGODB_DB_PORT' + process.env.OPENSHIFT_MONGODB_DB_PORT);
 
 var db;
 var uri = 'mongodb://node:node$1234@mongodb:27017/node';
